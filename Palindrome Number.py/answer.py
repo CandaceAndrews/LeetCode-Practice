9991cf4824063af2af11c58cb40cc19938fbf83e
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-04-03 18:53:36 -0400

    finished Palindrome Number

--- Palindrome Number.py/answer.py
+++ Palindrome Number.py/answer.py
@@ -4,3 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        x_string = str(x)
+        backwards_number = x_string[::-1]
+
+        if backwards_number == x_string:
+            return True
+        else:
+            return False
+
+
+# Test
+solution = Solution()
+solution.isPalindrome(121)
