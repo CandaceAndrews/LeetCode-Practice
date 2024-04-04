ae4ca1cd0c21f5fb9be658e420befbcc8245e5cc
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-04-04 18:49:02 -0400

    Roman to Integer

---
+++ Roman to Integer/answer.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        I = 1
+        V = 5
+        X = 10
+        L = 50
+        C = 100
+        M = 1000
