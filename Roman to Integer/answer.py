ee00cdb81dc7dc96ad982b468fb97c378abb2283
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-04-05 18:39:45 -0400

    Roman to Integer

--- Roman to Integer/answer.py
+++ Roman to Integer/answer.py
@@ -4,9 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        I = 1
-        V = 5
-        X = 10
-        L = 50
-        C = 100
-        M = 1000
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+
+        for i in range(len(s) - 1, -1, -1):
+            value = roman_values[s[i]]
+
+
+# solution = Solution()
+# solution.romanToInt('XX')
